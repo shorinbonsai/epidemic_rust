d3d81b0f838a07b2588e7efb0d4573a330c52de4
James Sargant <shorinbonsai@gmail.com>
2021-10-21 21:17:36 -0400

    most basic graph functionality working

--- src/graph.rs
+++ src/graph.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 #[derive(Debug, Copy, Clone)]
 pub struct Edge {
     pub to: usize,
@@ -14,21 +16,66 @@ impl Edge {
 /// An unweighted graph represented by an unweighted adjacency list.
 #[derive(Debug)]
 pub struct Unweighted {
-    graph: Vec<Vec<usize>>,
+    adj_list: Vec<Vec<usize>>,
 }
 
 impl Unweighted {
     pub fn init(n: usize) -> Self {
         Self {
-            graph: vec![vec![]; n],
+            adj_list: vec![vec![]; n],
         }
     }
 
+    pub fn new_undirected(size: usize, edges: &[[usize; 2]]) -> Self {
+        let mut graph = Self::init(size);
+        for &[a, b] in edges.iter() {
+            graph.add_undirected_edge(a, b);
+        }
+        graph
+    }
+
+    pub fn clear_graph(&mut self) {
+        for x in self.adj_list.iter_mut() {
+            x.clear();
+        }
+    }
+
+    pub fn kill_graph(&mut self) {
+        self.adj_list.clear()
+    }
+
     pub fn is_empty(&self) -> bool {
-        self.graph.is_empty()
+        self.adj_list.is_empty()
     }
 
     pub fn add_edge(&mut self, u: usize, v: usize) {
-        self.graph[u].push(v)
+        self.adj_list[u].push(v)
+    }
+
+    pub fn add_undirected_edge(&mut self, u: usize, v: usize) {
+        self.add_edge(u, v);
+        self.add_edge(v, u);
+    }
+
+    pub fn remove_edge(&mut self, u: usize, v: usize) {
+        if let Some(pos) = self.adj_list[u].iter().position(|x| *x == v) {
+            self.adj_list[u].remove(pos);
+        }
+    }
+
+    pub fn remove_edges(&mut self, u: usize, v: usize) {
+        self.remove_edge(u, v);
+        self.remove_edge(v, u);
+    }
+
+    pub fn infected(n: u32, alpha: f64) -> u32 {
+        let mut rng = rand::thread_rng();
+        let beta: f64 = 1.0 - (f64::from(n) * (1.0 - alpha).ln()).exp();
+        let x = rng.gen::<f64>();
+        if x < beta {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,43 @@
+use crate::graph::Unweighted;
 mod graph;
 
 fn main() {
     println!("Hello, world!");
+    let mut graph = Unweighted::new_undirected(
+        13,
+        &[
+            [0, 7],
+            [0, 9],
+            [0, 11],
+            [7, 11],
+            [7, 6],
+            [7, 3],
+            [6, 5],
+            [3, 4],
+            [2, 3],
+            [2, 12],
+            [12, 8],
+            [8, 1],
+            [1, 10],
+            [10, 9],
+            [9, 8],
+        ],
+    );
+
+    println!("{:?}", graph);
+    graph.remove_edge(0, 11);
+    println!("{:?}", graph);
+    graph.remove_edge(11, 0);
+    println!("{:?}", graph);
+    graph.remove_edges(0, 7);
+    println!("{:?}", graph);
+    graph.add_edge(0, 11);
+    graph.add_undirected_edge(7, 0);
+    graph.add_undirected_edge(7, 0);
+    println!("{:?}", graph);
+    println!("{:?}", graph.is_empty());
+    graph.clear_graph();
+    println!("{:?}", graph);
+    graph.kill_graph();
+    println!("{:?}", graph.is_empty());
 }
